b05aecc8f36a909f693261a9c21b588d65ef0b2e
aniskchaou <kchaouanis26@gmail.com>
2021-01-07 22:33:54 +0100

    change ui

--- src/app/modules/employee/employee/employee.component.ts
+++ src/app/modules/employee/employee/employee.component.ts
@@ -14,9 +14,14 @@ export class EmployeeComponent implements OnInit {
      '../assets/scripts/jquery-1.9.1.min.js',
      '../assets/scripts/jquery-ui-1.10.1.custom.min.js',
      '../assets/bootstrap/js/bootstrap.min.js',
-     '../assets/scripts/datatables/jquery.dataTables.js',
+     //'../assets/scripts/datatables/jquery.dataTables.js',
      '../assets/scripts/init.js',
-     //'https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js'
+     'https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js',
+     'https://cdn.datatables.net/v/dt/b-1.6.5/b-flash-1.6.5/b-html5-1.6.5/datatables.min.js',
+     'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js',
+     'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js',
+     'https://cdn.datatables.net/v/dt/dt-1.10.23/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/datatables.min.js'
+
     ];
     for (let i = 0; i < dynamicScripts.length; i++) {
       const node = document.createElement('script');
